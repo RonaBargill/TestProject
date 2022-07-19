12083b29bf1e3dbf5d16a612d3950907aa6bb4ff
RonaBargill <ronalalesko@gmail.com>
2022-07-19 16:13:14 +0300

    delete not relevant files

--- src/Main/java/main.java
+++ src/Main/java/main.java
@@ -1,7 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        SolutionForPalindrome solutionForPalindrome = new SolutionForPalindrome();
-        String solution = solutionForPalindrome.longestPalindrome("aaaaa");
 
 
     }
